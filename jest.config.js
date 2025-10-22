335a28e413836323dbd6a3852d5f27662d6a5952
jaspreet <jkaur126@academic.rrc.ca>
2025-10-22 18:13:48 -0500

    Update jestconfig for mocking tests and add setupmock.ts file for loan and admin routes test

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,10 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  moduleFileExtensions: ["ts", "js", "json"],
+  roots: ["<rootDir>/test"],
+  setupFilesAfterEnv: [],
+  verbose: true,
+  detectOpenHandles: true,
+  forceExit: true,
 };
